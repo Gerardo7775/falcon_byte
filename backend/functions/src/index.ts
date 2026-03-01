cdc6e24d77b100accd509fafb0005bad45bef3ea
Gerardo Adan Morelos Medina <88841180+Gerardo7775@users.noreply.github.com>
2026-03-01 00:33:02 -0600

    refactor: creacion y migracion a arquitectura back end

---
+++ backend/functions/src/index.ts
@@ -0,0 +1,8 @@
+import * as admin from 'firebase-admin';
+
+// Inicializar de forma global la SDK de Admin
+admin.initializeApp();
+
+// Exportar módulos (Aquí se registrarán todas las Cloud Functions)
+// export * from './triggers/onUserCreated';
+// export * from './services/generarVoucher';
